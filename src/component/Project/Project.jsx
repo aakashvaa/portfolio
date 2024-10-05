f0f25e8cf2d6d8077d5d42ec0ab959c7accbc818
AkashKumar-0 <ahmvaad@gmail.com>
2024-10-05 17:11:45 +0530

    project

--- src/component/Project/Project.jsx
+++ src/component/Project/Project.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import { BlurBlue, BlurWhite, ProjectSection } from './project.styles'
-import ProjectOne from './ProjectCard/ProjectOne.jsx'
-import ProjectTwo from './ProjectCard/ProjectTwo.jsx'
-import ProjectThree from './ProjectCard/ProjectThree.jsx'
-import ProjectFour from './ProjectCard/ProjectFour.jsx'
-import ProjectFive from './ProjectCard/ProjectFive.jsx'
-import ProjectSix from './ProjectCard/ProjectSix.jsx'
-import ProjectSeven from './ProjectCard/ProjectSeven.jsx'
-import ProjectEigth from './ProjectCard/ProjectEigth.jsx'
+import React from "react";
+import { BlurBlue, BlurWhite, ProjectSection } from "./project.styles";
+import ProjectOne from "./ProjectCard/ProjectOne.jsx";
+import ProjectTwo from "./ProjectCard/ProjectTwo.jsx";
+import ProjectThree from "./ProjectCard/ProjectThree.jsx";
+import ProjectFour from "./ProjectCard/ProjectFour.jsx";
+import ProjectFive from "./ProjectCard/ProjectFive.jsx";
+import ProjectSix from "./ProjectCard/ProjectSix.jsx";
+import ProjectSeven from "./ProjectCard/ProjectSeven.jsx";
+import ProjectEigth from "./ProjectCard/ProjectEigth.jsx";
 
 function Project() {
   return (
@@ -15,22 +15,15 @@ function Project() {
       <h1 className=" w-full text-center text-[3em]">Projects</h1>
       <BlurBlue />
       <BlurWhite />
-      <div className="relative text-sm justify-center items-center flex flex-col gap-y-4 w-full h-full flex-wrap ">
-        <ProjectOne />
-        <ProjectTwo />
-        <ProjectThree />
-        <ProjectFour />
-        <div className="sm:absolute lg:z-30 z-0  w-[250px] flex flex-col  sm:justify-between sm:h-[95%] lg:h-[70%] ">
-          <ProjectSeven />
-          <ProjectEigth />
-        </div>
-        <div className="sm:absolute z-20 sm:w-[60%] sm:h-[180px] flex flex-col sm:flex-row justify-between items-center ">
-          <ProjectFive />
-          <ProjectSix />
-        </div>
-      </div>
+      <main className="grid grid-rows-3 h-[300px] place-content-between  gap-x-10 gap-y-3 place-items-between grid-cols-5 ">
+        <div className="row-span-3 col-span-1 h-full  w-[100px]  rounded-md bg-[rgba(255,255,255,0.03)] backdrop-blur-xl   ]"></div>
+        <div className=" row-span-1 w-full col-span-3 rounded-md bg-[rgba(255,255,255,0.03)] backdrop-blur-xl"></div>
+        <div className=" row-span-3 col-span-1 h-full w-[100px] rounded-md  bg-[rgba(255,255,255,0.03)] backdrop-blur-xl"></div>
+        <div className="row-span-1 w-full col-span-3 bg-[rgba(255,255,255,0.03)] backdrop-blur-xl rounded-md"></div>
+        <div className="row-span-1 w-full col-span-3  bg-[rgba(255,255,255,0.03)] backdrop-blur-xl rounded-md"></div>
+      </main>
     </ProjectSection>
-  )
+  );
 }
 
-export default Project
+export default Project;
